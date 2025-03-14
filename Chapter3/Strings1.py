397bb115d11ff4aa0ec6880a0e03c75987616908
Andrew Schwartz <abenschwartz02@gmail.com>
2025-03-14 06:19:52 -0400

    Strings1

---
+++ Chapter3/Strings1.py
@@ -0,0 +1,57 @@
+print("double quotes print")
+print('sing quotes print')
+#Digit as String!
+print('12536')
+
+print("She said, \"Hello Andrew\"")
+print('it\'s a nice day')
+
+print('A\nN\nD\nR\nE\nW')
+
+print("Backwords Slash: \\")
+print("2 Backwords Slashes: \\\\")
+
+print(r'this\n is\n a\n raw\n string\n')
+
+print("\nPrinting a Triple qutoes string:")
+
+print("""
+  Usage: things[OPTIONS]
+      -h                    Display this usage message
+      -H hostname           Hostname to connect
+""")
+
+print("3 * un: " + (3 * 'un'))
+print("3 * un + ium: " + (3 * 'un' + 'ium'))
+
+print("printing 'py' 'thon': " 'py' 'thon')
+
+print("\nString")
+print("String[0]: " + "String"[0])
+print("String[1]: " + "String"[1])
+print("String[2]: " + "String"[2])
+print("String[3]: " + "String"[3])
+print("String[4]: " + "String"[4])
+print("String[5]: " + "String"[5])
+
+print("\nString[-1]: " + "String"[-1])
+print("String[-2]: " + "String"[-2])
+print("String[-3]: " + "String"[-3])
+print("String[-4]: " + "String"[-4])
+print("String[-5]: " + "String"[-5])
+print("String[-6]: " + "String"[-6])
+
+print("String[0:2]: " + "String"[0:2])
+print("String[2:6]: " + "String"[2:6])
+print("String[:2]: " + "String"[:2])
+print("String[2:]: " + "String"[2:])
+print("String[:i] + String[:i]: Always will return full " + "String"[:4] + "String"[4:])
+
+unchangable = "Im forever"
+print("word[0] = 'A' Results in an error")
+
+print("len('Andrew'): " + str(len('Andrew')))
+
+
+
+
