fdededcb7fba1b7051d5e114ba374909eb6459ce
Andrew Schwartz <abenschwartz02@gmail.com>
2025-03-14 05:47:03 -0400

    Basic Calculations

---
+++ Calculator1.py
@@ -0,0 +1,35 @@
+# This is a comment :)
+print("Doing Basic Math")
+print(2 + 2)
+print(50 - 5*6)
+print((50 - 5*6) / 4)
+print(8 / 5)
+print((50 - 5) * 6)
+
+
+print("\nRegular vs Floor Divsion: ")
+print(5 / 2)
+print(5 // 2)
+
+print("Remainder of 5 / 2: " + str(5 % 2))
+
+print("\nSquare Values:")
+print(1 ** 2)
+print(2 ** 2)
+print(3 ** 2)
+print(4 ** 2)
+print(5 ** 2)
+
+width = 20
+height = 10
+print("Area: " + str(width * height))
+
+
+bigDec = 12.6518341892418348956
+print("\nRoudning the number: " + str(bigDec))
+print(round(bigDec, 0))
+print(round(bigDec, 1))
+print(round(bigDec, 2))
+print(round(bigDec, 3))
+print(round(bigDec, 4))
+print(round(bigDec, 5))
